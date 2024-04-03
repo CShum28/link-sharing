f1b56b89b84f69225f348c3d65fa254a38b0764c
Clement <clement.shum128@gmail.com>
2024-04-02 23:49:52 -0400

    base page

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,35 @@
+// "use client";
+
+import Logo from "../components/Logo";
+import PrimaryButton from "@/components/Button/PrimaryButton";
+// import { useRouter } from "next/navigation";
+
+import { SignInButton, SignOutButton, auth } from "@clerk/nextjs";
+
+export default function Home() {
+  const { userId } = auth();
+
+  // const router = useRouter();
+
+  // const handleClick = () => {
+  //   router.push("/login");
+  // };
+
+  return (
+    <main className="min-h-screen mobile:p-8 tablet:p-24 desktop:p-36 flex flex-col">
+      <div className="flex-shrink-0">
+        <Logo />
+      </div>
+      <div className="flex-grow flex items-center justify-center">
+        {userId ? (
+          <SignOutButton className="text-white bg-primary h-12 rounded-md w-full" />
+        ) : (
+          <SignInButton
+            afterSignInUrl="/customize-link"
+            className="text-white bg-primary h-12 rounded-md w-full"
+          />
+        )}
+      </div>
+    </main>
+  );
+}
