92551d8c6c60d4d16cc37e7b399ff1a714bb5933
Clement <clement.shum128@gmail.com>
2024-04-02 23:50:24 -0400

    primary button

---
+++ my-app/src/components/Button/PrimaryButton.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface PrimaryButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({
+  children,
+  onClick,
+  disabled,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={
+        "text-white h-12 rounded-md w-full border-2 " +
+        `${
+          disabled
+            ? "bg-disabled cursor-not-allowed"
+            : "bg-primary active:bg-active"
+        }`
+      }
+    >
+      {children}
+    </button>
+  );
+};
+
+export default PrimaryButton;
