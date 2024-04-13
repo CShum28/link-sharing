e87c79781fa0ba3c2d0591031c7c1cbb2220902c
Clement <clement.shum128@gmail.com>
2024-04-13 00:26:35 -0400

    update button w. type

--- my-app/src/components/Button/PrimaryButton.tsx
+++ my-app/src/components/Button/PrimaryButton.tsx
@@ -2,17 +2,20 @@ import React from "react";
 
 interface PrimaryButtonProps {
   children: React.ReactNode;
+  type?: "submit" | "reset" | "button" | undefined;
   onClick?: () => void;
   disabled?: boolean;
 }
 
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({
   children,
+  type,
   onClick,
   disabled,
 }) => {
   return (
     <button
+      type={type}
       onClick={onClick}
       className={
         "text-white h-12 rounded-lg w-full border-2 " +
