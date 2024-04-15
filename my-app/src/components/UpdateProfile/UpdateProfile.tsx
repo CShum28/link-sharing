f26650c55f091c1bb63fe4a11d7b8508e4a7c08c
Clement <clement.shum128@gmail.com>
2024-04-15 19:46:57 -0400

    create form

--- my-app/src/components/UpdateProfile/UpdateProfile.tsx
+++ my-app/src/components/UpdateProfile/UpdateProfile.tsx
@@ -13,7 +13,7 @@ interface UpdateProfileProps {
   profile: Profile;
   onTextChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onFileChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  submit: () => void;
+  submit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
 const UpdateProfile: React.FC<UpdateProfileProps> = ({
@@ -24,62 +24,64 @@ const UpdateProfile: React.FC<UpdateProfileProps> = ({
 }) => {
   return (
     <div className="text-secondaryText">
-      <div className="bg-background p-6 rounded-md my-8">
-        <p>Profile picture</p>
-        <label className="flex flex-col justify-center items-center h-48 w-48 bg-disabled rounded-lg hover:cursor-pointer my-6">
-          <input
-            type="file"
-            name="photo"
-            onChange={onFileChange}
-            className="hidden"
-          />
-          <Image
-            src="/image.png"
-            width={40}
-            height={40}
-            alt={"picture of image"}
-          />
-          <p className="text-primary font-semibold mt-3">+ Upload Image</p>
-        </label>
-        <p className="text-xs">
-          Image must be below 1024x1024px. Use PNG or FPG format.
-        </p>
-      </div>
-      <div className="bg-background p-6 rounded-md">
-        <div className="mb-4">
-          <p>First name*</p>
-          <input
-            name="firstName"
-            type="text"
-            className="w-full p-3 border-2 rounded-md"
-            value={profile.firstName}
-            onChange={onTextChange}
-          />
+      <form onSubmit={submit}>
+        <div className="bg-background p-6 rounded-md my-8">
+          <p>Profile picture</p>
+          <label className="flex flex-col justify-center items-center h-48 w-48 bg-disabled rounded-lg hover:cursor-pointer my-6">
+            <input
+              type="file"
+              name="photo"
+              onChange={onFileChange}
+              className="hidden"
+            />
+            <Image
+              src="/image.png"
+              width={40}
+              height={40}
+              alt={"picture of image"}
+            />
+            <p className="text-primary font-semibold mt-3">+ Upload Image</p>
+          </label>
+          <p className="text-xs">
+            Image must be below 1024x1024px. Use PNG or FPG format.
+          </p>
         </div>
-        <div className="my-4">
-          <p>Last name*</p>
-          <input
-            name="lastName"
-            type="text"
-            className="w-full p-3 border-2 rounded-md"
-            value={profile.lastName}
-            onChange={onTextChange}
-          />
+        <div className="bg-background p-6 rounded-md">
+          <div className="mb-4">
+            <p>First name*</p>
+            <input
+              name="firstName"
+              type="text"
+              className="w-full p-3 border-2 rounded-md"
+              value={profile.firstName}
+              onChange={onTextChange}
+            />
+          </div>
+          <div className="my-4">
+            <p>Last name*</p>
+            <input
+              name="lastName"
+              type="text"
+              className="w-full p-3 border-2 rounded-md"
+              value={profile.lastName}
+              onChange={onTextChange}
+            />
+          </div>
+          <div>
+            <p>Email</p>
+            <input
+              name="email"
+              type="email"
+              className="w-full p-3 border-2 rounded-md"
+              value={profile.email}
+              onChange={onTextChange}
+            />
+          </div>
         </div>
-        <div>
-          <p>Email</p>
-          <input
-            name="email"
-            type="email"
-            className="w-full p-3 border-2 rounded-md"
-            value={profile.email}
-            onChange={onTextChange}
-          />
+        <div className="border-t-2 p-3 bg-background rounded-b-lg">
+          <PrimaryButton children="Save" type="submit" />
         </div>
-      </div>
-      <div className="border-t-2 p-3 bg-background rounded-b-lg">
-        <PrimaryButton children="Save" onClick={submit} />
-      </div>
+      </form>
     </div>
   );
 };
