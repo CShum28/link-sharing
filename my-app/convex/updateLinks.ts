fded698d975f14ef9bfcff71d7abfacd01b238e3
Clement <clement.shum128@gmail.com>
2024-04-15 19:46:12 -0400

    updateLink mutation

--- my-app/convex/updateLinks.ts
+++ my-app/convex/updateLinks.ts
@@ -10,12 +10,28 @@ export const updateLinks = mutation({
         platform: v.string(),
         link: v.string(),
         placeholder: v.string(),
+        // All the below are needed to remove validator error
+        userId: v.optional(v.any()),
+        _id: v.optional(v.any()),
+        _creationTime: v.optional(v.any()),
       })
     ),
   },
   handler: async (ctx, args) => {
+    // Get array of existing links
+    const existingLinks = await ctx.db
+      .query("links")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .collect();
+
+    // Loop over array of existing links and delete them all
+    for (const link of existingLinks) {
+      await ctx.db.delete(link._id);
+    }
+
     const links = args.links;
 
+    // // // Array of new links
     const insertPromise = links.map((linkInfo) =>
       ctx.db.insert("links", {
         userId: args.userId,
@@ -26,6 +42,7 @@ export const updateLinks = mutation({
       })
     );
 
+    // // // Promise that returns all of the new links
     const insertLinks = await Promise.all(insertPromise);
     return insertLinks;
   },
