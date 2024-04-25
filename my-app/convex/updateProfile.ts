4133d706ef01baab7438d616451b80b6609e092c
Clement <clement.shum128@gmail.com>
2024-04-24 22:59:59 -0400

    add generateUploadUrl

--- my-app/convex/updateProfile.ts
+++ my-app/convex/updateProfile.ts
@@ -1,21 +1,23 @@
 import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
+export const generateUploadUrl = mutation(async (ctx) => {
+  return await ctx.storage.generateUploadUrl();
+});
+
 export const updateProfile = mutation({
   args: {
     userId: v.string(),
-    profile: v.array(
-      v.object({
-        photo: v.bytes(),
-        firstName: v.string(),
-        lastName: v.string(),
-        email: v.string(),
-        // // All the below are needed to remove validator error
-        // userId: v.optional(v.any()),
-        // _id: v.optional(v.any()),
-        // _creationTime: v.optional(v.any()),
-      })
-    ),
+    profile: v.object({
+      photo: v.any(),
+      firstName: v.string(),
+      lastName: v.string(),
+      email: v.string(),
+      // // All the below are needed to remove validator error
+      // userId: v.optional(v.any()),
+      // _id: v.optional(v.any()),
+      // _creationTime: v.optional(v.any()),
+    }),
   },
   handler: async (ctx, args) => {
     console.log(args.profile);
