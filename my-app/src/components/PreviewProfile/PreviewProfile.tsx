76a486f011e23be833ba3463e2239d5f1237d260
Clement <clement.shum128@gmail.com>
2024-04-24 23:01:25 -0400

    Preview Profile page

---
+++ my-app/src/components/PreviewProfile/PreviewProfile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+const PreviewProfile = () => {
+  return (
+    <div>
+      <p>Preview Profile</p>
+    </div>
+  );
+};
+
+export default PreviewProfile;
