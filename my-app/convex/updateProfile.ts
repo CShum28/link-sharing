44f29ecbd4315d1fd48ba88c064cbe97ed6cac91
Clement <clement.shum128@gmail.com>
2024-05-01 21:00:01 -0400

    update profile in convex

--- my-app/convex/updateProfile.ts
+++ my-app/convex/updateProfile.ts
@@ -1,6 +1,7 @@
 import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
+// Used to generate string for image uploaded, so it can be stored in db
 export const generateUploadUrl = mutation(async (ctx) => {
   return await ctx.storage.generateUploadUrl();
 });
@@ -9,17 +10,38 @@ export const updateProfile = mutation({
   args: {
     userId: v.string(),
     profile: v.object({
-      photo: v.any(),
+      photo: v.string(),
       firstName: v.string(),
       lastName: v.string(),
       email: v.string(),
-      // // All the below are needed to remove validator error
-      // userId: v.optional(v.any()),
-      // _id: v.optional(v.any()),
-      // _creationTime: v.optional(v.any()),
     }),
   },
   handler: async (ctx, args) => {
-    console.log(args.profile);
+    // this is to check if a profile currently exists or not
+    const existingProfile = await ctx.db
+      .query("profiles")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .first();
+
+    if (existingProfile) {
+      // if profile already exists, just update it in db
+      await ctx.db.patch(existingProfile._id, {
+        photo: args.profile.photo,
+        firstName: args.profile.firstName,
+        lastName: args.profile.lastName,
+        email: args.profile.email,
+      });
+      console.log("Profile updated");
+    } else {
+      // if profile does not exist, create a new profile in db
+      await ctx.db.insert("profiles", {
+        userId: args.userId,
+        photo: args.profile.photo,
+        firstName: args.profile.firstName,
+        lastName: args.profile.lastName,
+        email: args.profile.email,
+      });
+      console.log("Profile added");
+    }
   },
 });
