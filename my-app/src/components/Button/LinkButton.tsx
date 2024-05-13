429032e2478ddcf50f8eeef1a7fe1c6d99e10f70
Clement <clement.shum128@gmail.com>
2024-05-13 19:53:06 -0400

    link button

--- my-app/src/components/Button/LinkButton.tsx
+++ my-app/src/components/Button/LinkButton.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 
 interface LinkButtonProps {
-  name: string;
+  platform: string;
+  color: string;
 }
 
-const LinkButton: React.FC<LinkButtonProps> = ({ name }) => {
+const LinkButton: React.FC<LinkButtonProps> = ({ platform, color }) => {
+  console.log(color);
   return (
     <div className="my-3">
-      <button className="flex flex-row w-60 border-2 justify-between py-3 px-4 rounded-lg">
-        <div>
-          <p className="capitalize">{name}</p>
+      <button
+        className="flex flex-row w-60 justify-between py-3 px-4 rounded-lg"
+        style={{ backgroundColor: `#${color}` }}
+      >
+        <div className="flex flex-row justify-center items-center gap-2">
+          <Image
+            src={`/LinkLogo/${platform}.png`}
+            width={20}
+            height={10}
+            alt="logo icon"
+          />
+          <p className="capitalize text-white">{platform}</p>
         </div>
-        <ArrowRight />
+        <ArrowRight className="text-white" />
       </button>
     </div>
   );
