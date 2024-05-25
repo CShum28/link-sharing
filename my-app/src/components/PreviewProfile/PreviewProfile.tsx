b9f5332c920e5e6919f064b6bba63084230ddf00
Clement <clement.shum128@gmail.com>
2024-05-25 01:13:49 -0400

    preview profile page

--- my-app/src/components/PreviewProfile/PreviewProfile.tsx
+++ my-app/src/components/PreviewProfile/PreviewProfile.tsx
@@ -1,12 +1,60 @@
-import React from "react";
-import Image from "next/image";
+"use client";
 
-const PreviewProfile = () => {
+import React, { useEffect } from "react";
+import { api } from "../../../convex/_generated/api";
+import { useQuery } from "convex/react";
+import { useUser } from "@clerk/nextjs";
+import LinkButton from "@/components/Button/LinkButton";
+
+export default function ProfilePreview() {
+  // Get existing user through clerk
+  const { user } = useUser();
+
+  // Query for profile data (if it exists)
+  const profileData = useQuery(api.getProfiles.getProfiles, {
+    userId: user?.id || "",
+  });
+
+  // Query for ink data
+  const linksData = useQuery(api.getLinks.getLinks, { userId: user?.id || "" });
+
   return (
-    <div>
-      <p>Preview Profile</p>
-    </div>
+    <main>
+      <div className="border-2 p-10 rounded-3xl">
+        {profileData && linksData ? (
+          <div className="flex flex-col items-center tablet:border-2">
+            {/* Profile image */}
+            <img
+              src={profileData ? (profileData[0]?.imageUrl as string) : ""}
+              alt={profileData ? "Profile Photo" : ""}
+              className="h-48 w-48 rounded-full"
+            />
+            {/* Profile Name */}
+            <div className="my-3">
+              <p className="text-4xl font-semibold">
+                {profileData[0]?.firstName}&nbsp;{profileData[0]?.lastName}
+              </p>
+            </div>
+            {/* Profile email */}
+            <div className="mb-8">
+              <p>{profileData[0]?.email}</p>
+            </div>
+            {/* List of links */}
+            {linksData.map((link, index) => (
+              <LinkButton
+                key={index}
+                platform={link.platform}
+                color={link.color}
+                link={link.link}
+              />
+            ))}
+          </div>
+        ) : (
+          <div>
+            <p>Loading...</p>
+          </div>
+        )}
+      </div>
+    </main>
   );
-};
-
-export default PreviewProfile;
+}
