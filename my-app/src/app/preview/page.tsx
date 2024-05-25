0a09c3ac3cd650bb9f596438bc0a4f96b48e175e
Clement <clement.shum128@gmail.com>
2024-05-25 01:12:57 -0400

    remove comments, update css

--- my-app/src/app/preview/page.tsx
+++ my-app/src/app/preview/page.tsx
@@ -18,45 +18,43 @@ export default function Preview() {
   // Query for ink data
   const linksData = useQuery(api.getLinks.getLinks, { userId: user?.id || "" });
 
-  useEffect(() => {
-    console.log(profileData);
-    console.log(linksData);
-    // console.log(user);
-  }, [profileData, linksData, user]);
-
   return (
-    <main>
-      {profileData && linksData ? (
-        <div className="flex flex-col items-center">
-          {/* Profile image */}
-          <img
-            src={profileData ? (profileData[0]?.imageUrl as string) : ""}
-            alt={profileData ? "Profile Photo" : ""}
-            className="h-48 w-48 rounded-full"
-          />
-
-          {/* Profile Name */}
-          <div className="my-3">
-            <p className="text-4xl font-semibold">
-              {profileData[0].firstName}&nbsp;{profileData[0].lastName}
-            </p>
+    <main className="flex items-center justify-center">
+      <div className="bg-white w-fit p-8 rounded-3xl">
+        {profileData && linksData ? (
+          <div className="flex flex-col items-center tablet:border-2">
+            {/* Profile image */}
+            <img
+              src={profileData ? (profileData[0]?.imageUrl as string) : ""}
+              alt={profileData ? "Profile Photo" : ""}
+              className="h-48 w-48 rounded-full"
+            />
+            {/* Profile Name */}
+            <div className="my-3">
+              <p className="text-4xl font-semibold">
+                {profileData[0]?.firstName}&nbsp;{profileData[0]?.lastName}
+              </p>
+            </div>
+            {/* Profile email */}
+            <div className="mb-8">
+              <p>{profileData[0]?.email}</p>
+            </div>
+            {/* List of links */}
+            {linksData.map((link, index) => (
+              <LinkButton
+                key={index}
+                platform={link.platform}
+                color={link.color}
+                link={link.link}
+              />
+            ))}
           </div>
-
-          {/* Profile email */}
-          <div className="mb-8">
-            <p>{profileData[0].email}</p>
+        ) : (
+          <div>
+            <p>Loading...</p>
           </div>
-
-          {/* List of links */}
-          {linksData.map((link) => (
-            <LinkButton platform={link.platform} color={link.color} /> // Assuming you have or add a 'key' prop
-          ))}
-        </div>
-      ) : (
-        <div>
-          <p>Loading...</p>
-        </div>
-      )}
+        )}
+      </div>
     </main>
   );
 }
