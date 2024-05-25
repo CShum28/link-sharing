b5035b319e6e08e1ed2d10c8290d4f1b08cce26c
Clement <clement.shum128@gmail.com>
2024-05-25 01:13:28 -0400

    onclick open new page

--- my-app/src/components/Button/LinkButton.tsx
+++ my-app/src/components/Button/LinkButton.tsx
@@ -5,15 +5,27 @@ import Image from "next/image";
 interface LinkButtonProps {
   platform: string;
   color: string;
+  link: string;
 }
 
-const LinkButton: React.FC<LinkButtonProps> = ({ platform, color }) => {
-  console.log(color);
+const openInNewTab =
+  (url: string) => (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    // Ensure the URL is absolute
+    const absoluteUrl =
+      url.startsWith("http://") || url.startsWith("https://")
+        ? url
+        : `http://${url}`;
+    window.open(absoluteUrl, "_blank");
+  };
+
+const LinkButton: React.FC<LinkButtonProps> = ({ platform, color, link }) => {
   return (
     <div className="my-3">
       <button
         className="flex flex-row w-60 justify-between py-3 px-4 rounded-lg"
         style={{ backgroundColor: `#${color}` }}
+        onClick={openInNewTab(link)}
       >
         <div className="flex flex-row justify-center items-center gap-2">
           <Image
