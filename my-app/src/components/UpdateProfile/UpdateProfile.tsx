2805cb4c886eef55e463af6f76e9d42b1bb0f4be
Clement <clement.shum128@gmail.com>
2024-05-25 01:13:54 -0400

    update css

--- my-app/src/components/UpdateProfile/UpdateProfile.tsx
+++ my-app/src/components/UpdateProfile/UpdateProfile.tsx
@@ -26,100 +26,99 @@ const UpdateProfile: React.FC<UpdateProfileProps> = ({
   return (
     <div className="text-secondaryText">
       <form onSubmit={submit}>
-        <div className="bg-background p-6 rounded-md my-8">
-          <p>Profile picture</p>
+        <div className="bg-background p-6 rounded-md my-8 tablet:flex desktop:flex tablet:items-center desktop:items-center tablet:justify-between desktop:justify-between">
+          <div className="desktop:w-4/12">
+            <p>Profile picture</p>
+          </div>
 
-          {profile?.photo ? (
-            // If profile photo exists, display this
-            <>
-              <label className="flex flex-col justify-center items-center h-48 w-48 bg-disabled rounded-lg hover:cursor-pointer my-6">
-                <input
-                  type="file"
-                  name="photo"
-                  onChange={onFileChange}
-                  className="hidden"
-                />
-                {/* <Image
-                  src={profile.photoPreview}
-                  alt="Profile Photo"
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{
-                    width: "100%",
-                    height: "auto",
-                    borderRadius: "10px",
-                  }}
-                /> */}
-                <img
-                  src={profile.photoPreview}
-                  alt="Profile Photo"
-                  style={{
-                    width: "100%",
-                    height: "auto",
-                    borderRadius: "10px",
-                  }}
-                />
-              </label>
-            </>
-          ) : (
-            // If profile photo does not exist yet, display this
-            <>
-              <label className="flex flex-col justify-center items-center h-48 w-48 bg-disabled rounded-lg hover:cursor-pointer my-6">
-                <input
-                  type="file"
-                  name="photo"
-                  onChange={onFileChange}
-                  className="hidden"
-                />
-                <Image
-                  src="/image.png"
-                  width={40}
-                  height={40}
-                  alt={"picture of image"}
-                />
-                <p className="text-primary font-semibold mt-3">
-                  + Upload Image
-                </p>
-              </label>
-            </>
-          )}
-          <p className="text-xs">
-            Image must be below 1024x1024px. Use PNG or FPG format.
-          </p>
+          <div className="tablet:flex desktop:w-8/12 tablet:w-8/12 desktop:flex tablet:items-center desktop:items-center tablet:gap-4 desktop:gap-4">
+            {profile?.photo ? (
+              // If profile photo exists, display this
+              <>
+                <label className="flex flex-col justify-center items-center h-48 w-48 rounded-lg hover:cursor-pointer my-6">
+                  <input
+                    type="file"
+                    name="photo"
+                    onChange={onFileChange}
+                    className="hidden"
+                  />
+                  <img
+                    src={profile.photoPreview}
+                    alt="Profile Photo"
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                      borderRadius: "10px",
+                    }}
+                  />
+                </label>
+              </>
+            ) : (
+              // If profile photo does not exist yet, display this
+              <>
+                <label className="flex flex-col justify-center items-center h-48 w-48 bg-disabled rounded-lg hover:cursor-pointer my-6">
+                  <input
+                    type="file"
+                    name="photo"
+                    onChange={onFileChange}
+                    className="hidden"
+                  />
+                  <Image
+                    src="/image.png"
+                    width={40}
+                    height={40}
+                    alt={"picture of image"}
+                  />
+                  <p className="text-primary font-semibold mt-3">
+                    + Upload Image
+                  </p>
+                </label>
+              </>
+            )}
+            <p className="text-xs tablet:w-36 desktop:w-36">
+              Image must be below 1024x1024px. Use PNG or FPG format.
+            </p>
+          </div>
         </div>
+
         <div className="bg-background p-6 rounded-md">
           {/* First Name */}
-          <div className="mb-4">
-            <p>First name*</p>
+          <div className="mb-4 tablet:flex desktop:flex">
+            <div className="desktop:w-4/12 tablet:w-4/12">
+              <p>First name*</p>
+            </div>
             <input
               name="firstName"
               type="text"
-              className="w-full p-3 border-2 rounded-md"
+              className="mobile:w-full tablet:grow desktop:grow p-3 border-2 rounded-md"
               value={profile?.firstName}
               onChange={onTextChange}
             />
           </div>
 
           {/* Last Name */}
-          <div className="my-4">
-            <p>Last name*</p>
+          <div className="mb-4 tablet:flex desktop:flex">
+            <div className="desktop:w-4/12 tablet:w-4/12">
+              <p>Last name*</p>
+            </div>
             <input
               name="lastName"
               type="text"
-              className="w-full p-3 border-2 rounded-md"
+              className="mobile:w-full tablet:grow desktop:grow p-3 border-2 rounded-md"
               value={profile?.lastName}
               onChange={onTextChange}
             />
           </div>
 
           {/* Email */}
-          <div>
-            <p>Email*</p>
+          <div className="mb-4 tablet:flex desktop:flex">
+            <div className="desktop:w-4/12 tablet:w-4/12">
+              <p>Email*</p>
+            </div>
             <input
               name="email"
               type="email"
-              className="w-full p-3 border-2 rounded-md"
+              className="mobile:w-full tablet:grow desktop:grow p-3 border-2 rounded-md"
               value={profile?.email}
               onChange={onTextChange}
             />
